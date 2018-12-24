6732c0391d46ed23aa733c0cc38d25ea2aa5a4bd
imam harir <imamharir@gmail.com>
2018-12-24 16:41:36 +0000

    Create cluster.js

---
+++ cluster.js
@@ -0,0 +1,52 @@
+import cluster from 'cluster';
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+
+const wrap = (file)=>{
+    if (cluster.isMaster) {
+        const cpus = os.cpus().length;
+
+        for (let i = 0; i < cpus; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            if (code !== 0 && !worker.exitedAfterDisconnect) {
+                console.log(`Worker ${worker.id} crashed, Starting a new worker...`);
+                cluster.fork();
+            }
+        });
+
+        process.on("SIGUSR2", () => {
+            const workers = Object.values(cluster.workers);
+            const restartWorker = (workerIndex) => {
+                const worker = workers[workerIndex];
+                if (!worker) return;
+                worker.on('exit', () => {
+                    if (!worker.exitedAfterDisconnect) return;
+                    console.log(`Exited process ${worker.process.pid}`);
+                    cluster.fork().on('listening', () => {
+                        restartWorker(workerIndex + 1)
+                    });
+                });
+                worker.disconnect();
+            };
+            restartWorker(0);
+        })
+    } else {
+        require(file);
+    }
+};
+
+if(!module.parent) {
+    const appDir = path.dirname(require.main.filename);
+    const file = path.join(appDir,'..', process.argv[2]);
+
+    if (!fs.existsSync(file))
+        throw new Error(`File not exist: ${ file }`);
+    wrap(file);
+}
+
+
+export default wrap;
